18e002128487cd0630a7f28c0d30197cae3c8781
daniellew712 <xiaoyuew66@gmail.com>
2025-06-10 11:42:46 -0700

    wave01 updates

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,21 +2,35 @@ import PropTypes from 'prop-types';
 import Task from './Task.jsx';
 import './TaskList.css';
 
+// const TaskList = ({ tasks }) => {
+//   const getTaskListJSX = (tasks) => {
+//     return tasks.map((task) => {
+//       return (
+//         <Task
+//           key={task.id}
+//           id={task.id}
+//           title={task.title}
+//           isComplete={task.isComplete}
+//         />
+//       );
+//     });
+//   };
+//   return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+// };
+
 const TaskList = ({ tasks }) => {
-  const getTaskListJSX = (tasks) => {
-    return tasks.map((task) => {
-      return (
+  return(
+    <ul className="tasks__list no-bullet">
+      {tasks.map((task) => (
         <Task
           key={task.id}
           id={task.id}
           title={task.title}
           isComplete={task.isComplete}
         />
-      );
-    });
-  };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
-};
+      ))}
+    </ul>
+  );};
 
 TaskList.propTypes = {
   tasks: PropTypes.arrayOf(
